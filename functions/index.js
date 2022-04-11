7bf8548e641ce3f9f63b4e9e34ecd8260b8da56e
JoshJAL <JoshuaALevine3@gmail.com>
2022-04-11 01:21:18 -0400

    added backend

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,41 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51Kn8drK8xboc1Y4qWCsTFdolQL0jzBIOJlkhjaOw9h0ca5fPpnHexuR993wji3aB2mKvUh5V6jjJmxk2QXdjngIy00sypLSmmU"
+);
 
+// API
+
+// App config
+
+const app = express();
+
+// Middlewares
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+  console.log("Payment request received: ", total);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency (cents in this case)
+    currency: "usd",
+  });
+
+  res.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// Listeners
+
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint
+// http://localhost:5001/clone-ff644/us-central1/api
